e5d183a8377b2bce656e4ad16c0369cf3e4f159a
Harold Levid <hlmarin2003@gmail.com>
2022-08-05 11:35:44 -0500

    Modelos y relaciones

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -5,19 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Continent extends Model
+class Region extends Model
 {
     //la tabla con la cual el modelo se relaciona
-    protected $table = "continents";
+    protected $table = "regions";
     //la calve primaria de la tabla
-    protected $primaryKey="continent_id";
+    protected $primaryKey="region_id";
     //anular campos de auditoria
     public $timestamps = false;
     use HasFactory;
 
-    //relacion entre continente y region
-    public function Regiones(){
-        return $this->hasMany(Region::class,
-                              'Continent_id');
+    //relacion entre region y paises 1:m
+    public function paises(){
+        return $this->hasMany(country::class,
+                                'region_id');
     }
 }
