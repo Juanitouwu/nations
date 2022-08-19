371ce76203266132a1fa0cca34fc53d9ca3904a9
Harold Levid <hlmarin2003@gmail.com>
2022-08-19 10:36:57 -0500

    ejercicios 3 hechos

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -19,5 +19,12 @@ class Country extends Model
     public function region(){
         return $this->belongsTo(Region::class,
                                 'region_id');
+                                
+    }
+    public function idiomas(){
+        return $this->belongsToMany(Idioma::class,
+                                    'country_languages',
+                                    'country_id',
+                                    'language_id')->withPivot('official');
     }
 }
